72b45371bb6ccc9fb4437abdde35166209581ef5
gregoryjohncarter <gregoryjohncarter@gmail.com>
2022-10-22 15:25:56 -0400

    posts by user query w/o display

--- userPosts.js
+++ userPosts.js
@@ -1,7 +1,7 @@
 async function fetchUsersJsonPlaceholder(url) {
   fetch('https://jsonplaceholder.typicode.com/' + url)
   .then(response => response.json())
-  .then(test => displayUsers(test))
+  .then(json => displayUsers(json))
 }
 
 function displayUsers(userData) {
@@ -14,7 +14,7 @@ function displayUsers(userData) {
     var username = result.username;
     var email = result.email;
     
-    html += '<tr class="u ' + result.id + '"><td><span class="user">U: ' + username + '</span>';
+    html += '<tr class="u id' + result.id + '"><td><span class="user">U: ' + username + '</span>';
     html += '<br>';
     html += '<span class="email">E: ' + email + '</span></td></tr>';
   });
@@ -24,6 +24,22 @@ function displayUsers(userData) {
 
   let userSelect = document.querySelector('.user-select');
   userSelect.appendChild(userTable);
+
+  // after creating the list of users, go back and add on click functionality for each one
+  function enableSelectors(userData) {
+    userData.forEach((result) => {
+      const postsSelector = document.querySelector('.id' + String(result.id));
+      postsSelector.addEventListener('click', function() { selectPostsByUser(String(result.id)) })
+    })
+  }
+
+  enableSelectors(userData);
 }
 
 fetchUsersJsonPlaceholder('users');
+
+async function selectPostsByUser(id) {
+    fetch('https://jsonplaceholder.typicode.com/posts?userId=' + id)
+    .then(response => response.json())
+    .then(test => console.log(test))
+}
